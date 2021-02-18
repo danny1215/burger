ec2ee5d2ac9bc41b7ebd29f3252f2cc2fdebf924
daniel mestawat <dannymamusha@gmail.com>
2021-02-18 16:19:51 -0500

    add process.env

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'password',
     database: 'burgers_db'
   });
+
+};
+
   
   
   // Make connection.
